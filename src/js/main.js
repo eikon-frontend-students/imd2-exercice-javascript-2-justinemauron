c9b0aef0ae4f256783e983659941f46298679432
justinemauron <justine.mauron@studentfr.ch>
2025-01-30 09:38:57 +0100

    ajout des variable-js

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+var addToCartButtons = document.querySelectorAll(".product-addtocart");
+
+var confirmation = document.querySelector(".confirmation");
+
+addToCartButtons.forEach(function (addToCartButton) {});
+
+//setTimeout(() => {
+//confirmation.classList.remove("is-active");
+//}, 2000);
